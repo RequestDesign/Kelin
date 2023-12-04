e9738e7abc75afe4693196bf80fcd8a3dbb53b52
Olga Semenova <olga110723@gmail.com>
2023-12-04 22:24:50 +0300

    fix: main page

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -216,7 +216,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const phoneInputs = document.querySelectorAll('input[type="tel"]');
 
         phoneInputs.forEach((phoneInput) => {
-            const im = new Inputmask('+ 7 ( 999 ) 999-99-99', { placeholder: '+ 7 ( 999 ) 999-99-99' });
+            const im = new Inputmask('+ 7 ( 999 )  999 - 99 - 99');
             im.mask(phoneInput);
         });
     }
