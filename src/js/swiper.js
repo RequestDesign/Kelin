30323cc4636d5872f0042efe2fa7c61c21922c30
Olga Semenova <olga110723@gmail.com>
2023-12-04 19:16:25 +0300

    fix: build

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,178 +1,176 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
-        let swiper;
+const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
+    let swiper;
 
-        breakpoint = window.matchMedia(breakpoint);
+    breakpoint = window.matchMedia(breakpoint);
 
-        const enableSwiper = function (className, settings) {
-            swiper = new Swiper(className, settings);
+    const enableSwiper = function (className, settings) {
+        swiper = new Swiper(className, settings);
 
-            if (callback) {
-                callback(swiper);
-            }
-        };
-
-        const checker = function () {
-            if (breakpoint.matches) {
-                return enableSwiper(swiperClass, swiperSettings);
-            } else {
-                if (swiper !== undefined) swiper.destroy(true, true);
-                return;
-            }
-        };
-
-        breakpoint.addEventListener('change', checker);
-        checker();
+        if (callback) {
+            callback(swiper);
+        }
     };
-    const rem = function (rem) {
-        if ($(window).width() > 768) {
-            return 0.005208335 * $(window).width() * rem;
+
+    const checker = function () {
+        if (breakpoint.matches) {
+            return enableSwiper(swiperClass, swiperSettings);
         } else {
-            // где 375 это ширина моб версии макета
-            return (100 / 375) * (0.1 * $(window).width()) * rem;
+            if (swiper !== undefined) swiper.destroy(true, true);
+            return;
         }
     };
 
-    resizableSwiper('(max-width: 768px)', '.abilities-swiper', {
-        loop: true,
-        speed: 1200,
-        direction: 'horizontal',
-        slidesPerView: 'auto',
-        spaceBetween: rem(0.8)
-    });
+    breakpoint.addEventListener('change', checker);
+    checker();
+};
+const rem = function (rem) {
+    if ($(window).width() > 768) {
+        return 0.005208335 * $(window).width() * rem;
+    } else {
+        // где 375 это ширина моб версии макета
+        return (100 / 375) * (0.1 * $(window).width()) * rem;
+    }
+};
 
-    resizableSwiper('(min-width: 0px)', ' .skills-industry__industry-swiper', {
-        loop: true,
-        speed: 1200,
-        direction: 'horizontal',
-        slidesPerView: 1.3,
-        spaceBetween: rem(0.8),
-        grabCursor: true,
-        navigation: {
-            nextEl: '.skills-industry__industry-swiper-button-next'
+resizableSwiper('(max-width: 768px)', '.abilities-swiper', {
+    loop: true,
+    speed: 1200,
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: rem(0.8)
+});
+
+resizableSwiper('(min-width: 0px)', ' .skills-industry__industry-swiper', {
+    loop: true,
+    speed: 1200,
+    direction: 'horizontal',
+    slidesPerView: 1.3,
+    spaceBetween: rem(0.8),
+    grabCursor: true,
+    navigation: {
+        nextEl: '.skills-industry__industry-swiper-button-next'
+    },
+    breakpoints: {
+        768: {
+            direction: 'vertical',
+            spaceBetween: rem(1.2),
+            slidesPerView: 3
+        }
+    }
+});
+
+resizableSwiper('(max-width: 768px)', '.clients-swiper', {
+    loop: true,
+    speed: 1200,
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: rem(0.8)
+});
+
+resizableSwiper('(min-width: 0px)', '.protection-swiper', {
+    loop: true,
+    speed: 1200,
+    slidesPerView: 1,
+    grabCursor: true,
+    spaceBetween: rem(2),
+    navigation: {
+        nextEl: '.protection-swiper-button-next',
+        prevEl: '.protection-swiper-button-prev'
+    }
+});
+
+resizableSwiper('(min-width: 0px)', '.tu-letters-swiper', {
+    loop: true,
+    speed: 1200,
+    grabCursor: true,
+    spaceBetween: rem(1.2),
+    slidesPerView: 1.2,
+    navigation: {
+        nextEl: '.tu-letters-swiper-button-next',
+        prevEl: '.tu-letters-swiper-button-prev'
+    },
+    breakpoints: {
+        768: {
+            spaceBetween: rem(1.6),
+            slidesPerView: 3
+        }
+    }
+});
+
+resizableSwiper('(max-width: 768px)', '.talents-swiper', {
+    loop: true,
+    speed: 1200,
+    direction: 'horizontal',
+    slidesPerView: 1.2,
+    spaceBetween: rem(0.8)
+});
+
+resizableSwiper('(min-width: 0px)', '.individual-calc-swiper', {
+    loop: true,
+    speed: 1200,
+    grabCursor: true,
+    spaceBetween: rem(0.8),
+    slidesPerView: 'auto',
+
+    breakpoints: {
+        768: {
+            spaceBetween: rem(1.6),
+            slidesPerView: 2
+        }
+    }
+});
+
+resizableSwiper('(min-width: 0px)', '.category-list-swiper', {
+    loop: true,
+    speed: 1200,
+    grabCursor: true,
+    spaceBetween: rem(0.8),
+    slidesPerView: 'auto',
+
+    breakpoints: {
+        768: {
+            spaceBetween: rem(1.2)
+        }
+    }
+});
+
+resizableSwiper('(min-width: 0px)', '.reviews-swiper', {
+    loop: true,
+    speed: 1200,
+    grabCursor: true,
+    spaceBetween: rem(0.8),
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.reviews-button-next',
+        prevEl: '.reviews-button-prev'
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: rem(1.2)
+        }
+    }
+});
+
+new Swiper('.cases-swiper', {
+    speed: 1200,
+    grabCursor: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: rem(2.4)
         },
-        breakpoints: {
-            768: {
-                direction: 'vertical',
-                spaceBetween: rem(1.2),
-                slidesPerView: 3
+        768: {
+            slidesPerView: 2,
+            spaceBetween: rem(1.2),
+            grid: {
+                rows: 2,
+                fill: 'row'
             }
         }
-    });
-
-    resizableSwiper('(max-width: 768px)', '.clients-swiper', {
-        loop: true,
-        speed: 1200,
-        direction: 'horizontal',
-        slidesPerView: 'auto',
-        spaceBetween: rem(0.8)
-    });
-
-    resizableSwiper('(min-width: 0px)', '.protection-swiper', {
-        loop: true,
-        speed: 1200,
-        slidesPerView: 1,
-        grabCursor: true,
-        spaceBetween: rem(2),
-        navigation: {
-            nextEl: '.protection-swiper-button-next',
-            prevEl: '.protection-swiper-button-prev'
-        }
-    });
-
-    resizableSwiper('(min-width: 0px)', '.tu-letters-swiper', {
-        loop: true,
-        speed: 1200,
-        grabCursor: true,
-        spaceBetween: rem(1.2),
-        slidesPerView: 1.2,
-        navigation: {
-            nextEl: '.tu-letters-swiper-button-next',
-            prevEl: '.tu-letters-swiper-button-prev'
-        },
-        breakpoints: {
-            768: {
-                spaceBetween: rem(1.6),
-                slidesPerView: 3
-            }
-        }
-    });
-
-    resizableSwiper('(max-width: 768px)', '.talents-swiper', {
-        loop: true,
-        speed: 1200,
-        direction: 'horizontal',
-        slidesPerView: 1.2,
-        spaceBetween: rem(0.8)
-    });
-
-    resizableSwiper('(min-width: 0px)', '.individual-calc-swiper', {
-        loop: true,
-        speed: 1200,
-        grabCursor: true,
-        spaceBetween: rem(0.8),
-        slidesPerView: 'auto',
-
-        breakpoints: {
-            768: {
-                spaceBetween: rem(1.6),
-                slidesPerView: 2
-            }
-        }
-    });
-
-    resizableSwiper('(min-width: 0px)', '.category-list-swiper', {
-        loop: true,
-        speed: 1200,
-        grabCursor: true,
-        spaceBetween: rem(0.8),
-        slidesPerView: 'auto',
-
-        breakpoints: {
-            768: {
-                spaceBetween: rem(1.2)
-            }
-        }
-    });
-
-    resizableSwiper('(min-width: 0px)', '.reviews-swiper', {
-        loop: true,
-        speed: 1200,
-        grabCursor: true,
-        spaceBetween: rem(0.8),
-        slidesPerView: 1,
-        navigation: {
-            nextEl: '.reviews-button-next',
-            prevEl: '.reviews-button-prev'
-        },
-        breakpoints: {
-            768: {
-                slidesPerView: 2,
-                spaceBetween: rem(1.2)
-            }
-        }
-    });
-
-    new Swiper('.cases-swiper', {
-        speed: 1200,
-        grabCursor: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-                spaceBetween: rem(2.4)
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: rem(1.2),
-                grid: {
-                    rows: 2,
-                    fill: 'row'
-                }
-            }
-        },
-        navigation: {
-            nextEl: '.cases-next',
-            prevEl: '.cases-prev'
-        }
-    });
+    },
+    navigation: {
+        nextEl: '.cases-next',
+        prevEl: '.cases-prev'
+    }
 });
